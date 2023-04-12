cb25ce062014d8da227805204360d2303156bcd5
Charles Albert <charles.ccomp@gmail.com>
2023-04-12 08:59:52 -0300

    criaçao da atividade para exibiçao de pessoas

---
+++ app/src/main/java/br/edu/ifto/formpessoa/InfoPessoa.java
@@ -0,0 +1,14 @@
+package br.edu.ifto.formpessoa;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InfoPessoa extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info_pessoa);
+    }
+}

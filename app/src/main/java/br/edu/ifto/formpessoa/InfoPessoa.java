b30bfe350a3c8ba1a8c00f4155353881666cb594
Charles Albert <charles.ccomp@gmail.com>
2023-04-12 12:56:00 -0300

    inicio da implementacao da visualizaçao de detalhes da pessoa

--- app/src/main/java/br/edu/ifto/formpessoa/InfoPessoa.java
+++ app/src/main/java/br/edu/ifto/formpessoa/InfoPessoa.java
@@ -3,6 +3,10 @@ package br.edu.ifto.formpessoa;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import br.edu.ifto.formpessoa.entities.Pessoa;
 
 public class InfoPessoa extends AppCompatActivity {
 
@@ -10,5 +14,30 @@ public class InfoPessoa extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info_pessoa);
+        getSupportActionBar().setDisplayHomeAsUpEnabled( true );
+
+        Pessoa pessoa = (Pessoa) getIntent().getSerializableExtra("pessoa");
+
+        TextView detalhesPessoa = findViewById(R.id.detalhesPessoa);
+        detalhesPessoa.append("\n");
+        detalhesPessoa.append("Dados da Pessoa\n");
+        detalhesPessoa.append("Nome: "+pessoa.getNome()+"\n");
+        detalhesPessoa.append("Idade: "+pessoa.getIdade()+" anos\n");
+        detalhesPessoa.append("Genero: "+pessoa.getSexo().toString()+"\n");
+        detalhesPessoa.append("Dados da Pessoa\n");
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            //identificar a ação de voltar a tela
+            case android.R.id.home:
+                //encerra a activity
+                finish();
+                break;
+        }
+
+        return super.onOptionsItemSelected( item );
     }
 }
--- app/src/main/java/br/edu/ifto/formpessoa/MainActivity.java
+++ app/src/main/java/br/edu/ifto/formpessoa/MainActivity.java
@@ -2,7 +2,22 @@ package br.edu.ifto.formpessoa;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import br.edu.ifto.formpessoa.entities.Pessoa;
+import br.edu.ifto.formpessoa.enums.EstiloMusical;
+import br.edu.ifto.formpessoa.enums.Sexo;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +25,63 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.bt_enviar);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent infoPessoa = new Intent(getActivity(),InfoPessoa.class);
+                EditText nome = findViewById(R.id.input_nome);
+                EditText idade = findViewById(R.id.input_idade);
+                RadioGroup rgsexo = findViewById(R.id.rg_sexo);
+                Sexo sexo = Sexo.OUTRO;
+                switch (rgsexo.getCheckedRadioButtonId()) {
+                    case (R.id.rb_sexo_masculino):
+                        sexo = Sexo.MASCULINO;
+                        break;
+                    case (R.id.rb_sexo_feminino):
+                        sexo = Sexo.FEMININO;
+                        break;
+                    case (R.id.rb_sexo_outro):
+                        sexo = Sexo.OUTRO;
+                        break;
+                }
+                List<EstiloMusical> estilosMusicais = new ArrayList<>();
+                CheckBox cb_rock = findViewById(R.id.cb_rock);
+                if(cb_rock.isChecked()){
+                    estilosMusicais.add(EstiloMusical.ROCK);
+                }
+                CheckBox cb_sertanejo = findViewById(R.id.cb_sertanejo);
+                if(cb_sertanejo.isChecked()){
+                    estilosMusicais.add(EstiloMusical.SERTANEJO);
+                }
+                CheckBox cb_pagode = findViewById(R.id.cb_pagode);
+                if(cb_pagode.isChecked()){
+                    estilosMusicais.add(EstiloMusical.PAGODE);
+                }
+                CheckBox cb_forro = findViewById(R.id.cb_forro);
+                if(cb_forro.isChecked()){
+                    estilosMusicais.add(EstiloMusical.FORRO);
+                }
+                CheckBox cb_outros = findViewById(R.id.cb_outros);
+                if(cb_outros.isChecked()){
+                    estilosMusicais.add(EstiloMusical.OUTRO);
+                }
+                Pessoa pessoa = new Pessoa(
+                        nome.getText().toString(),
+                        Integer.parseInt(idade.getText().toString()),
+                        sexo,
+                        estilosMusicais
+                );
+                infoPessoa.putExtra("pessoa", pessoa);
+                startActivity(infoPessoa);
+            }
+        });
+
     }
+
+    public Context getActivity(){
+        return this;
+    }
+
 }
